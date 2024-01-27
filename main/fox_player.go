17c927c9fcb92e4818d76e4e69513d121e86b799
Titouan Ravard <titouan.ravard@etu.univ-nantes.fr>
2024-01-27 22:38:28 +0100

    add sound jump for fox

--- main/fox_player.go
+++ main/fox_player.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "jump"
+  component: "/assests/sound/sound_effect/jump_1.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
