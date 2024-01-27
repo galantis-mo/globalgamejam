2b614b5c3bb01369ab95301e65972430e8e3aced
Katel Hignard <E214928S>
2024-01-27 09:09:21 +0100

    Modification des formes de collisions

--- main/fairy_player.go
+++ main/fairy_player.go
@@ -65,7 +65,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/images/characters.atlas\"\n"
-  "default_animation: \"f\\303\\251e\"\n"
+  "default_animation: \"fairy\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
