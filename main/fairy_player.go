9632a7159a9b3803038e55928130ea321dab4eea
galantis-mo <rimmonique@gmail.com>
2024-01-27 15:45:03 +0100

    Fairy grab action

--- main/fairy_player.go
+++ main/fairy_player.go
@@ -1,5 +1,5 @@
 components {
-  id: "pikachu_control"
+  id: "fairy_control"
   component: "/scripts/player_fairy.script"
   position {
     x: 0.0
@@ -22,6 +22,7 @@ embedded_components {
   "friction: 0.0\n"
   "restitution: 0.0\n"
   "group: \"default\"\n"
+  "mask: \"grabbable\"\n"
   "mask: \"default\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
@@ -40,9 +41,9 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 129.17046\n"
-  "  data: 124.812744\n"
-  "  data: 10.192\n"
+  "  data: 287.76736\n"
+  "  data: 402.09387\n"
+  "  data: 125.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
@@ -68,6 +69,54 @@ embedded_components {
   "default_animation: \"fairy\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "trigger"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.0\n"
+  "restitution: 0.0\n"
+  "group: \"default\"\n"
+  "mask: \"grabbable\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -172.0\n"
+  "      y: 92.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 287.76736\n"
+  "  data: 402.09387\n"
+  "  data: 125.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
   ""
   position {
     x: 0.0
