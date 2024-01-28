72cd694687fca44137bf21373b68c4263bb0b890
galantis-mo <rimmonique@gmail.com>
2024-01-28 10:01:56 +0100

    Ajout de la barre de rire

--- main/laugh_o_meter.go
+++ main/laugh_o_meter.go
@@ -19,7 +19,7 @@ embedded_components {
   data: "tile_set: \"/images/laugh_o_meter.atlas\"\n"
   "default_animation: \"laugh_o_meter_line\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "blend_mode: BLEND_MODE_ADD\n"
   ""
   position {
     x: 0.0
@@ -39,7 +39,14 @@ embedded_components {
   data: "tile_set: \"/images/laugh_o_meter.atlas\"\n"
   "default_animation: \"laugh_o_meter_01\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "blend_mode: BLEND_MODE_SCREEN\n"
+  "size {\n"
+  "  x: 575.0\n"
+  "  y: 1794.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
   ""
   position {
     x: 0.0
